a7f8bdefef12a7924e528f10dc8ddda5f90f6f53
AK <ndrkrpv@gmail.com>
2021-02-23 16:04:59 +0200

    Set up behat

---
+++ features/Context/FeatureContext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Behat;
+
+use Behat\MinkExtension\Context\RawMinkContext;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Define application featues from the specific context.
+ */
+class FeatureContext extends RawMinkContext {
+
+}
