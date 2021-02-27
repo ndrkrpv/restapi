f1b143952069a3d292eac58d8c5c9fb03346d704
AK <ndrkrpv@gmail.com>
2021-02-27 20:01:41 +0200

    Add positions controller

---
+++ src/Controller/PositionsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class PositionsController
+{
+    public function positionsAction(int $id): JsonResponse
+    {
+        $position = [
+            'id' => $id,
+            'jobTitle' => 'Senior PHP Developer',
+            'seniorityLevel' => 'Senior',
+            'country' => 'DE',
+            'city' => 'Berlin',
+            'salary' => 747500,
+            'currency' => 'SVU',
+            'requiredSkills' => 'PHP, Symfony, REST, Unit-testing, Behat, SOLID, Docker, AWS',
+            'companySize' => '100-500',
+            'companyDomain' => 'Automotive'
+        ];
+
+        return new JsonResponse($position, Response::HTTP_OK);
+    }
+
+}
