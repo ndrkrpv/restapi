6e1352e5edb532860cf7c2f94f3114963be0d2e3
AK <ndrkrpv@gmail.com>
2021-02-28 08:43:46 +0200

    Add position DTO

--- src/Controller/PositionController.php
+++ src/Controller/PositionController.php
@@ -2,27 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Dto\PositionDto;
+use App\Service\PositionService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class PositionController
 {
+    private PositionService $positionService;
+
+    public function __construct(PositionService $positionService) {
+        $this->positionService = $positionService;
+    }
+
+
     public function positionsAction(int $id): JsonResponse
     {
-        $position = [
-            'id' => $id,
-            'jobTitle' => 'Senior PHP Developer',
-            'seniorityLevel' => 'Senior',
-            'country' => 'DE',
-            'city' => 'Berlin',
-            'salary' => 747500,
-            'currency' => 'SVU',
-            'requiredSkills' => 'PHP, Symfony, REST, Unit-testing, Behat, SOLID, Docker, AWS',
-            'companySize' => '100-500',
-            'companyDomain' => 'Automotive'
-        ];
+        $position = $this->positionService->retrievePositionById($id);
+        $positionDto = PositionDto::fromEntity($position);
 
-        return new JsonResponse($position, Response::HTTP_OK);
+        return new JsonResponse($positionDto->toArray(), Response::HTTP_OK);
     }
 
 }
