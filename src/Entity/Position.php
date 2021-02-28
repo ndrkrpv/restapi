4a8e65cbb6a4d8dbf17b351cd0399083a11e20de
AK <ndrkrpv@gmail.com>
2021-02-28 07:17:00 +0200

    Add position entity

---
+++ src/Entity/Position.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Position
+{
+    private $id;
+
+    private $jobTitle;
+
+    private $seniorityLevel;
+
+    private $country;
+
+    private $city;
+
+    private $salary;
+
+    private $currency;
+
+    private $requiredSkills;
+
+    private $companySize;
+
+    private $companyDomain;
+
+}
